0045eacd6741d4a1a575ead416ef2d703b38f62d
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2022-09-16 22:43:41 -0300

    Clase Persona, estudiante y Profesor

--- src/programacion_de_alpicaciones/Programacion_de_Alpicaciones.java
+++ src/programacion_de_alpicaciones/Programacion_de_Alpicaciones.java
@@ -10,7 +10,9 @@ public class Programacion_de_Alpicaciones {
 
     
     public static void main(String[] args) {
-        System.out.println("XD");
+        //
+        
+        
         
     }
     
