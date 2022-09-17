90e5875db53842517a8c3d630e30ba743645969f
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2022-09-17 01:48:27 -0300

    Menu principal construido Clase para ingresar datos

--- src/programacion_de_aplicaciones/Ingresar.java
+++ src/programacion_de_aplicaciones/Ingresar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programacion_de_aplicaciones;
 
 import java.util.InputMismatchException;
@@ -26,16 +21,19 @@ public class Ingresar {
             int edad = s.nextInt();
             System.out.println("Si la persona a ingresar es Estudiante, ingrese \"E\"");
             System.out.print("Si es profesor, ingrese \"P\":\n");
-            String es = s.nextLine();
+            String es = s.next();
             if(es.equals("E")){
                 System.out.println("En que ramo va el estudiante");
-                String ramo = s.nextLine();
-                System.out.println("En que semestre va el estudiante");
+                String ramo = s.next();
+                System.out.println("En que semestre va el estudiante (solo numeros)");
                 int semestre = s.nextInt();
                 st = new Estudiante(semestre,nombre,apellido,edad,ramo);
                 st.guardar();
             }else if(es.equals("P")){
-                
+                System.out.println("¿que titulo tiene el profesor?");
+                String titulo = s.next();
+                pr = new Profesor(titulo,nombre, apellido,edad);
+                pr.guardar();
             }
             
         }catch(InputMismatchException e){
