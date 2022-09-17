60bab333859feefdeb3c696e5533e3a9e9dea194
Hector Ledezma <73298715+HectorLedezma@users.noreply.github.com>
2022-09-16 22:51:56 -0300

    Clase Persona, estudiante y Profesor

---
+++ src/programacion_de_aplicaciones/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion_de_aplicaciones;
+
+public class Persona {
+    String nombre;
+    String apellido;
+    int edad;
+    String ramo;
+}
---
+++ src/programacion_de_aplicaciones/Programacion_de_Aplicaciones.java
@@ -0,0 +1,8 @@
+package programacion_de_aplicaciones;
+public class Programacion_de_Aplicaciones {
+    public static void main(String[] args) {
+        System.out.println("XD");
+    }
+    //hola
+    
+}
