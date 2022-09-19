f8f5a68163d8d7193cd58970906fd65c7ee382ee
Sonvanie <113405775+Sonvanie@users.noreply.github.com>
2022-09-18 21:15:44 -0300

    Update Persona.java

--- src/programacion_de_aplicaciones/Persona.java
+++ src/programacion_de_aplicaciones/Persona.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package programacion_de_aplicaciones;
-
+//
 public class Persona {
     String nombre;
     String apellido;
     int edad;
-
+//
     public Persona(String nombre, String apellido, int edad) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -18,5 +13,4 @@ public class Persona {
     public void guardar(){
         
     }
-    
 }
