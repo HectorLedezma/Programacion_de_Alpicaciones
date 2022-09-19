8b57e786f015de197176ac4364594515a064e891
Sonvanie <113405775+Sonvanie@users.noreply.github.com>
2022-09-18 21:09:57 -0300

    Update Estudiante.java

--- src/programacion_de_aplicaciones/Estudiante.java
+++ src/programacion_de_aplicaciones/Estudiante.java
@@ -1,18 +1,16 @@
 package programacion_de_aplicaciones;
-
+//
 public class Estudiante extends Persona{
     int semestre;
     String ramo;
-
+//
     public Estudiante(int semestre, String nombre, String apellido, int edad, String ramo) {
         super(nombre, apellido, edad);
         this.semestre = semestre;
     }
-
+//
     public void setSemestre(int semestre) {
         this.semestre = semestre;
     }
-
-    
-    
+//   
 }
