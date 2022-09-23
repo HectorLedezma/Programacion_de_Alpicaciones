68335c5d1b13192818a58667d6a57be20b83f91b
Kensel <70713679+kensel01@users.noreply.github.com>
2022-09-23 15:14:35 -0300

    Co-authored-by: valfo13 <V-bit512@users.noreply.github.com>

--- src/programacion_de_aplicaciones/Buscar.java
+++ src/programacion_de_aplicaciones/Buscar.java
@@ -1,5 +1,30 @@
 package programacion_de_aplicaciones;
 
+import java.util.Scanner;
+
 public class Buscar {
+    boolean found;
+    Scanner s;
+    public Buscar() {
+	}
+    public void BuscarDatos(){
+        boolean found = false;
+        System.out.print("Ingrese nombre a buscar");
+        String nombre = s.nextLine();
+
+        i = c.iterator();
+        while(i.hasNext());
+            Persona e = i.next();
+            if(e.getNombre()==nombre){
+                System.out.println(e);
+                found=true
+            }
+        }
+        if (!found){
+            System.out.println("No se encontró registro");
     
+        }
+         System.out.println("-------------------------");
 }
+}
+
